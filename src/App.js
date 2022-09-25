b3044fe83ec8793a4d077fd3689389344a25387b
europeanplaice <paul.thomas.goodfellas@gmail.com>
2022-09-25 11:51:27 +0900

    get font

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+var JSZip = require("jszip");
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  showFile = async (e) => {
+    e.preventDefault()
+    const reader = new FileReader();
+    reader.onload = async (e1) => { 
+      const zip = new JSZip();
+      let t = await zip.loadAsync(e1.target.result)
+        .then((e) => e.files["ppt/slides/slide1.xml"].async("string").then((g) => g));
+        const parser = new DOMParser();
+        const dom = parser.parseFromString(t, "application/xml");
+        let data = dom.childNodes[0].childNodes[0].childNodes[0];
+        data = data.getElementsByTagNameNS("http://schemas.openxmlformats.org/presentationml/2006/main", "sp");
+        for (let d of data) {
+          d = d.getElementsByTagNameNS("http://schemas.openxmlformats.org/presentationml/2006/main", "txBody")[0];
+          d = d.getElementsByTagNameNS("http://schemas.openxmlformats.org/drawingml/2006/main", "p")[0];
+          d = d.getElementsByTagNameNS("http://schemas.openxmlformats.org/drawingml/2006/main", "r")[0];
+          if (!d){
+            continue
+          }
+          d = d.getElementsByTagNameNS("http://schemas.openxmlformats.org/drawingml/2006/main", "rPr")[0];
+          d = d.getElementsByTagNameNS("http://schemas.openxmlformats.org/drawingml/2006/main", "latin")[0];
+          if (!d){
+            console.log("default");
+            continue
+          }
+          d = d.getAttribute("typeface");
+          console.log(d);
+        }
+      };
+    reader.readAsBinaryString(e.target.files[0]);
+  }
+
+  render = () => {
+    return (<div>
+      <input type="file" onChange={(e) => this.showFile(e)} />
     </div>
-  );
+    )
+  }
 }
 
 export default App;
